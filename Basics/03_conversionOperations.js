f459640845c202f448b2208fc99c72a9c50764b0
vaibhavKondekar <143535454+vaibhavKondekar@users.noreply.github.com>
2025-02-02 05:04:47 +0000

    conversion Operations

---
+++ Basics/03_conversionOperations.js
@@ -0,0 +1,29 @@
+// let score= "vaibhav"
+
+// console.log(typeof(score));
+// // console.log(typeof typeof score);
+
+// let valuInNumber= Number(score)
+// console.log(valuInNumber);
+// console.log(typeof(valuInNumber));
+
+//"33" => 33
+//"33abc" =>NaN
+//true => 1
+//
+
+// let isLoggedIn=-5;
+// let booleanIsLoggedIn=Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn)
+
+//1=>true  except 0 for any number it will give you true
+//0=> false
+//"any string" =>true
+//"" => false;
+
+
+let someNumber =33;
+let str=String(someNumber);
+console.log(str);
+console.log(typeof str)
+
