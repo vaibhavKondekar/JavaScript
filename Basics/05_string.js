1033445e742163d20bf961fcd1c316774e2e90bf
vaibhavKondekar <143535454+vaibhavKondekar@users.noreply.github.com>
2025-02-02 09:19:13 +0000

    strings

---
+++ Basics/05_string.js
@@ -0,0 +1,36 @@
+const name="vaibhav"
+const age=20;
+
+//console.log("Hello my name is "+name+"and my age is "+age);
+
+console.log(`hello my name is ${ name}.and my age is ${age}.` )
+
+const gameName = new String('hitesh-hc-com')
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2))
+;
+console.log(gameName.indexOf('y'));
+
+const newString = gameName.substring(0, 4)
+console.log(newString);
+
+const anotherString = gameName.slice(-8, 4)
+console.log(anotherString);
+
+const newStringOne = "   hitesh    "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://hitesh.com/hitesh%20choudhary"
+
+console.log(url.replace('%20', '-'))
+
+console.log(url.includes('sundar'))
+
+console.log(gameName.split('-',2));
